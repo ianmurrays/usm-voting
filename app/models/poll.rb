4ea392c635069f78159ad48a8a660a0700b7952c
Ian Murray <ianmurrays@gmail.com>
2011-07-10 01:30:42 -0400

    Shows polls, open and closed, and close time

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,8 @@ class Poll < ActiveRecord::Base
   has_many :alternatives
   
   validates :question, :start_date, :finish_date, :presence => true
+  
+  def open?
+    self.finish_date > Time.now and Time.now > self.start_date
+  end
 end
